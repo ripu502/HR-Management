4829359f047485c4d03259bf24ff41cacb5bff56
Ripudaman Singh <ripu502@gmail.com>
2020-07-13 02:16:26 +0530

    keys.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 app.use(router);
 
 
-mongoose.connect(keys.mongoUri,
+mongoose.connect(`mongodb+srv://${process.env.MongoUser}:${process.env.MongoPassword}@cluster0-jywn3.mongodb.net/${process.env.MongoDatabase}?retryWrites=true`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
